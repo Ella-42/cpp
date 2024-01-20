e2ef351af0e2aaa34b09fe706db7f55df09e15e0
ella <ellakazam.h@gmail.com>
2024-01-20 08:16:53 +0100

    finished ex00 of cpp00 and made a makefile template that can easily be used for any file in the future for C++ files(auto_push)

--- test/test0.cpp
+++ test/test0.cpp
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.cpp                                           :+:      :+:    :+:   */
+/*   test0.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpeeters <lpeeters@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:38:07 by lpeeters          #+#    #+#             */
-/*   Updated: 2024/01/11 15:48:52 by lpeeters         ###   ########.fr       */
+/*   Updated: 2024/01/19 22:39:46 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-/* simple program that takes an input and displays it back on the output */
-
+//simple program that takes an input and displays it back on the output
 int	main(void)
 {
 	std::string	user_input; //define string
 
 	std::cout << "in: "; //output request
-	std::cin >> user_input; //request in and send it to string variable
+	std::getline(std::cin, user_input); //request in and send it to string variable
 	std::cout << "out: " << user_input << '\n'; //output the input
 }
