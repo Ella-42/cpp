48cf9da3792f53b9c00cec50c78b37e43fd06b06
Ella-42 <Ellakazam.h@gmail.com>
2024-02-15 23:05:39 +0100

    finished CPP04/ex00, honestly, spent way more time on the fucking test than anything else :skull:(auto_push)

---
+++ 4/ex00/class/Dog.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 18:08:38 by lpeeters          #+#    #+#             */
+/*   Updated: 2024/02/15 20:47:54 by lpeeters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+// Custom class: Dog
+class	Dog : public Animal
+{
+	public:
+		// Constructor
+		Dog(void);
+
+		// Copy constructor
+		Dog(const Dog& other);
+
+		// Copy assignment operator overload
+		Dog& operator = (const Dog& other);
+
+		// Make dog sounds
+		void makeSound(void) const;
+
+		// Destructor
+		~Dog(void);
+
+	protected:
+		;
+
+	private:
+		;
+};
+
+#endif
---
+++ 4/ex00/main.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 18:03:16 by lpeeters          #+#    #+#             */
+/*   Updated: 2024/02/15 22:13:51 by lpeeters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+/* Includes */
+
+# include <iostream> //input\output
+# include <string> //string
+			 
+/* Custom classes */
+
+# include "class/Animal.hpp" //Custom class: Animal
+# include "class/Dog.hpp" //Custom class: Dog
+# include "class/Cat.hpp" //Custom class: Cat
+# include "class/WrongAnimal.hpp" //Custom class: WrongAnimal
+# include "class/WrongCat.hpp" //Custom class: WrongCat
+					   
+/* Functions */
+
+// 
+
+
+#endif
