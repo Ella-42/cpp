55d95d3394d4302a04e7b384baf0cebd21f8c50d
Ella-42 <Ellakazam.h@gmail.com>
2024-03-08 21:55:12 +0100

    testing in main(auto push :heart:)

--- 1/ex03/main.cpp
+++ 1/ex03/main.cpp
@@ -6,46 +6,52 @@
 /*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:51:02 by lpeeters          #+#    #+#             */
-/*   Updated: 2024/02/07 20:44:08 by lpeeters         ###   ########.fr       */
+/*   Updated: 2024/02/29 19:38:37 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-// Testing
 int	main(void)
 {
-	// Testing no weapon for HumanB
-	HumanB subject_two("Lana");
-	subject_two.attack();
+	Weapon test = Weapon("test");
+	HumanA test_subject = HumanA("test_subject", test);
+}
 
-	// Winner of this battle
-	std::cout << "Well this seems mighty unfair, let's give Lana a chance.\n";
+// Testing
+//int	main(void)
+//{
+	// Testing no weapon for HumanB
+	//HumanB subject_two("Lana");
+	//subject_two.attack();
+
+	//// Winner of this battle
+	//std::cout << "Well this seems mighty unfair, let's give Lana a chance.\n";
 
 	// Testing HumanB
-	Weapon subject_two_weapon = Weapon("sword");
-	subject_two.setWeapon(subject_two_weapon);
-	subject_two.attack();
+	//Weapon subject_two_weapon = Weapon("sword");
+	//subject_two.setWeapon(subject_two_weapon);
+	//subject_two.attack();
 
-	// Testing HumanA
-	Weapon subject_one_weapon = Weapon("axe");
-	HumanA subject_one("Anna", subject_one_weapon);
-	subject_one.attack();
+	//// Testing HumanA
+	//Weapon subject_one_weapon = Weapon("axe");
+	//HumanA subject_one("Anna", subject_one_weapon);
+	//subject_one.attack();
 
-	// Winner of this battle
-	std::cout << "Lana wins!\n";
+	//// Winner of this battle
+	//std::cout << "Lana wins!\n";
 
-	// Testing weapon change for HumanA
-	subject_one_weapon.setType("hammer");
-	subject_one.attack();
+	//// Testing weapon change for HumanA
+	//subject_one_weapon.setType("hammer");
+	//subject_one.attack();
 
-	// Testing weapon change for HumanB
-	subject_two_weapon.setType("shovel");
-	subject_two.attack();
+	//// Testing weapon change for HumanB
+	//subject_two_weapon.setType("shovel");
+	//subject_two.attack();
 
-	// Winner of this battle
-	std::cout << "Anna wins!\n";
-}
+	//// Winner of this battle
+	//std::cout << "Anna wins!\n";
+//}
 
 //int main()
 //{
